9c335159607c2a28871fc54f83d763791ab7cd2f
AlexJanow <85886307+AlexJanow@users.noreply.github.com>
2021-07-05 14:39:39 +0200

    add exercise 2

--- 1-adding-elements/solution.js
+++ 1-adding-elements/solution.js
@@ -1,17 +1,14 @@
 let btn = document.querySelector(".button-add");
 let ctn = document.querySelector(".container");
-let box = document.querySelector(".box");
-let body = document.body;
-let div = document.createElement("div");
 
-btn.addEventListener("click", (e) => {
-  if (ctn === false) {
-    let newCont = document.createElement("ctn");
-    body.append(newCont);
-  } else {
-    let newBox = document.createElement("div");
-    ctn.append(newBox);
-    newBox.classList.add("box");
-    console.log("lol");
-  }
+btn.addEventListener("click", () => {
+  let newBox = document.createElement("div");
+  ctn.append(newBox);
+  newBox.classList.add("box");
+
+  newBox.addEventListener("click", () => {
+    if (confirm("Delete this box?")) {
+      newBox.remove();
+    }
+  });
 });
