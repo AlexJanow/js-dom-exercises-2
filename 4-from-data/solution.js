8e05148864ac5edfd82ded20a5ff3edf6399a6eb
AlexJanow <85886307+AlexJanow@users.noreply.github.com>
2021-07-05 22:23:31 +0200

    add exercise 4, turns out if the console tells me something is not a function...it is not a function.

--- 4-from-data/solution.js
+++ 4-from-data/solution.js
@@ -1,3 +1,6 @@
+let btn = document.querySelector(".button-add");
+let ctn = document.querySelector(".container");
+
 const elementsToRender = [
   {
     className: "box",
@@ -16,3 +19,17 @@ const elementsToRender = [
     backgroundColor: "lightseagreen",
   },
 ];
+
+btn.addEventListener("click", () => {
+  for (let i = 0; i < elementsToRender.length; i++) {
+    const boxData = elementsToRender[i];
+    const boxDataName = boxData.className;
+    const boxDataColor = boxData.backgroundColor;
+    const newBox = document.createElement("div");
+    newBox.classList.add(boxDataName);
+    newBox.style.backgroundColor = boxDataColor;
+    ctn.append(newBox);
+    console.log(boxDataName);
+    console.log(boxDataColor);
+  }
+});
